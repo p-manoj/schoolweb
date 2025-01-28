a09da50039d040ccbb85a3f7ca2c0e0daed2e643
manoj <some>
2025-01-28 11:47:38 +0530

    updated leaderboard

--- updateLeaderBoard.js
+++ updateLeaderBoard.js
@@ -4,15 +4,15 @@ import { getFirestore, collection, addDoc, getDocs, updateDoc } from "https://ww
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyAyuuJq22oR8VvXTzTLwmQQMuRY4_szxic",
-    authDomain: "project-quiz-31651.firebaseapp.com",
-    projectId: "project-quiz-31651",
-    storageBucket: "project-quiz-31651.firebasestorage.app",
-    messagingSenderId: "217705043693",
-    appId: "1:217705043693:web:e1b77947dcce37aec61ee4",
-    measurementId: "G-TYWXL7MS5S"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAyuuJq22oR8VvXTzTLwmQQMuRY4_szxic",
+//     authDomain: "project-quiz-31651.firebaseapp.com",
+//     projectId: "project-quiz-31651",
+//     storageBucket: "project-quiz-31651.firebasestorage.app",
+//     messagingSenderId: "217705043693",
+//     appId: "1:217705043693:web:e1b77947dcce37aec61ee4",
+//     measurementId: "G-TYWXL7MS5S"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
