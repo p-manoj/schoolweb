3f017a86d4c29aa91111f2043aa3d47dc35dfbd8
manoj <some>
2025-01-28 11:59:37 +0530

    updated leaderboard

--- updateLeaderBoard.js
+++ updateLeaderBoard.js
@@ -1,19 +1,10 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, updateDoc } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js";
+import firebaseConfig from "./config.js";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyAyuuJq22oR8VvXTzTLwmQQMuRY4_szxic",
-//     authDomain: "project-quiz-31651.firebaseapp.com",
-//     projectId: "project-quiz-31651",
-//     storageBucket: "project-quiz-31651.firebasestorage.app",
-//     messagingSenderId: "217705043693",
-//     appId: "1:217705043693:web:e1b77947dcce37aec61ee4",
-//     measurementId: "G-TYWXL7MS5S"
-// };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
