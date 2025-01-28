2c81c5ff96fbe32daf7254a5c60a777b896ae82d
manoj <some>
2025-01-28 12:21:10 +0530

    updated

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAyuuJq22oR8VvXTzTLwmQQMuRY4_szxic",
-    authDomain: "project-quiz-31651.firebaseapp.com",
-    projectId: "project-quiz-31651",
-    storageBucket: "project-quiz-31651.firebasestorage.app",
-    messagingSenderId: "217705043693",
-    appId: "1:217705043693:web:e1b77947dcce37aec61ee4",
-    measurementId: "G-TYWXL7MS5S"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAyuuJq22oR8VvXTzTLwmQQMuRY4_szxic",
+//     authDomain: "project-quiz-31651.firebaseapp.com",
+//     projectId: "project-quiz-31651",
+//     storageBucket: "project-quiz-31651.firebasestorage.app",
+//     messagingSenderId: "217705043693",
+//     appId: "1:217705043693:web:e1b77947dcce37aec61ee4",
+//     measurementId: "G-TYWXL7MS5S"
+// };
 
 export default firebaseConfig;
